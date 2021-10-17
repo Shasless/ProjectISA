c726407fc42389f8567608337c0b24b1712d258d
shasless <Efrei2018>
2021-10-17 13:24:06 +0200

    fix + improve

--- projet/MVM/View/CoinView.xaml.cs
+++ projet/MVM/View/CoinView.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Printing;
+using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
@@ -11,51 +13,68 @@ namespace projet.MVM.View
     public partial class CoinView : UserControl
     {
         public CoinView()
+        
         {
             InitializeComponent();
-      
-            MainWindow mw = (MainWindow) Application.Current.MainWindow;
+            Thread t = new Thread(new ThreadStart(ShowMetric));
+            t.Start();
+           
+
+
+
+
+        }
+
+        public async void ShowMetric()
+        {
+            
+           
+            this.Dispatcher.BeginInvoke(new Action(() =>  
+            {  
+                MainWindow mw = (MainWindow) Application.Current.MainWindow;
          
-            var p = new APIcontrol();
+                var p = new APIcontrol();
             
-            p.GetInfo(mw.Searchstr);
+                p.GetInfo(mw.Searchstr);
+                title.Text = p.objectRes.data[0].name;
+                price.Text= p.objectRes.data[0].price +" $";
+                mw.coin.Content = p.objectRes.data[0].name;
+                dayex.Text = p.objectRes.data[0].percent_change_24h +"% 24h";
+                montheex.Text = p.objectRes.data[0].percent_change_30d +"% 30d";
+                weekex.Text = p.objectRes.data[0].percent_change_7d +"% 7d";
+                dayex.Foreground = p.objectRes.data[0].percent_change_24h > 0 ? Brushes.GreenYellow: Brushes.Red;
+                montheex.Foreground = p.objectRes.data[0].percent_change_30d > 0 ? Brushes.GreenYellow: Brushes.Red;
+                weekex.Foreground = p.objectRes.data[0].percent_change_7d > 0 ? Brushes.GreenYellow: Brushes.Red;
+                bitprice.Text = p.objectRes.data[0].price_btc+" BTC";
+                sigle.Text = p.objectRes.data[0].symbol;
+
+                if (p.objectRes.data[0].market_cap > 1000000)
+                {
+                    CapMarket.Text = p.objectRes.data[0].market_cap > 1000000000
+                        ? ((int) (p.objectRes.data[0].market_cap / 1000000000)) + "B"
+                        : ((int) (p.objectRes.data[0].market_cap / 1000000)) + "M";
+                }
+                else
+                {
+                    CapMarket.Text = p.objectRes.data[0].market_cap.ToString();}
+                if (p.objectRes.data[0].volume > 1000000)
+                {
+                    VolMarket.Text = p.objectRes.data[0].volume > 1000000000
+                        ? ((int)(p.objectRes.data[0].volume / 1000000000))+ "B"
+                        : ((int)(p.objectRes.data[0].volume / 1000000))+ "M";
+                }else
+                {
+                    VolMarket.Text = p.objectRes.data[0].volume.ToString();}
+
+                GalaxySc.Text = p.objectRes.data[0].galaxy_score.ToString();
+                altrank.Text = p.objectRes.data[0].alt_rank.ToString();
+                //BitmapImage img = new BitmapImage(new Uri("https://dkhpfm5hits1w.cloudfront.net/" + p.objectRes.data[0].name + ".png"));
+                //Image.Source = img;
+  
+            })); 
+           
            
             
-           
-           
-            title.Text = p.objectRes.data[0].name;
-            price.Text= p.objectRes.data[0].price +" $";
-            mw.coin.Content = p.objectRes.data[0].name;
-            dayex.Text = p.objectRes.data[0].percent_change_24h +"% 24h";
-            montheex.Text = p.objectRes.data[0].percent_change_30d +"% 30d";
-            weekex.Text = p.objectRes.data[0].percent_change_7d +"% 7d";
-            dayex.Foreground = p.objectRes.data[0].percent_change_24h > 0 ? Brushes.Green: Brushes.Red;
-            montheex.Foreground = p.objectRes.data[0].percent_change_30d > 0 ? Brushes.Green: Brushes.Red;
-            weekex.Foreground = p.objectRes.data[0].percent_change_7d > 0 ? Brushes.Green: Brushes.Red;
-            bitprice.Text = p.objectRes.data[0].price_btc+" BTC";
-            sigle.Text = p.objectRes.data[0].symbol;
-
-            if (p.objectRes.data[0].market_cap > 1000000)
-            {
-                CapMarket.Text = p.objectRes.data[0].market_cap > 1000000000
-                    ? ((int)(p.objectRes.data[0].market_cap / 1000000000)) + "B"
-                    : ((int)(p.objectRes.data[0].market_cap / 1000000)) + "M";
-            }
-            if (p.objectRes.data[0].volume > 1000000)
-            {
-                VolMarket.Text = p.objectRes.data[0].volume > 1000000000
-                    ? ((int)(p.objectRes.data[0].volume / 1000000000))+ "B"
-                    : ((int)(p.objectRes.data[0].volume / 1000000))+ "M";
-            }
-
-            GalaxySc.Text = p.objectRes.data[0].galaxy_score.ToString();
-            altrank.Text = p.objectRes.data[0].alt_rank.ToString();
-            //BitmapImage img = new BitmapImage(new Uri("https://dkhpfm5hits1w.cloudfront.net/" + p.objectRes.data[0].name + ".png"));
-            //Image.Source = img;
-
-
-
-
 
         }
     }
