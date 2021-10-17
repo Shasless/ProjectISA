cdf44c4da66037a7275b264a5c0d2102506792a1
shasless <Efrei2018>
2021-10-17 11:50:44 +0200

    COIN page

--- projet/MVM/View/CoinView.xaml.cs
+++ projet/MVM/View/CoinView.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
 using projet.MVM.ViewWodel;
 using projet.APIcontroler;
 
@@ -17,9 +19,34 @@ namespace projet.MVM.View
             var p = new APIcontrol();
             p.GetInfo(mw.Searchstr);
             title.Text = p.objectRes.data[0].name;
-            a.Text= p.objectRes.data[0].price.ToString();
-            aa.Text= p.objectRes.data[0].symbol;
+            price.Text= p.objectRes.data[0].price +" $";
             mw.coin.Content = p.objectRes.data[0].name;
+            dayex.Text = p.objectRes.data[0].percent_change_24h +"% 24h";
+            montheex.Text = p.objectRes.data[0].percent_change_30d +"% 30d";
+            weekex.Text = p.objectRes.data[0].percent_change_7d +"% 7d";
+            dayex.Foreground = p.objectRes.data[0].percent_change_24h > 0 ? Brushes.Green: Brushes.Red;
+            montheex.Foreground = p.objectRes.data[0].percent_change_30d > 0 ? Brushes.Green: Brushes.Red;
+            weekex.Foreground = p.objectRes.data[0].percent_change_7d > 0 ? Brushes.Green: Brushes.Red;
+            bitprice.Text = p.objectRes.data[0].price_btc+" BTC";
+            sigle.Text = p.objectRes.data[0].symbol;
+
+            if (p.objectRes.data[0].market_cap > 1000000)
+            {
+                CapMarket.Text = p.objectRes.data[0].market_cap > 1000000000
+                    ? ((int)(p.objectRes.data[0].market_cap / 1000000000)) + "B"
+                    : ((int)(p.objectRes.data[0].market_cap / 1000000)) + "M";
+            }
+            if (p.objectRes.data[0].volume > 1000000)
+            {
+                VolMarket.Text = p.objectRes.data[0].volume > 1000000000
+                    ? ((int)(p.objectRes.data[0].volume / 1000000000))+ "B"
+                    : ((int)(p.objectRes.data[0].volume / 1000000))+ "M";
+            }
+
+            GalaxySc.Text = p.objectRes.data[0].galaxy_score.ToString();
+            altrank.Text = p.objectRes.data[0].alt_rank.ToString();
+            //BitmapImage img = new BitmapImage(new Uri("https://dkhpfm5hits1w.cloudfront.net/" + p.objectRes.data[0].name + ".png"));
+            //Image.Source = img;
 
 
 
